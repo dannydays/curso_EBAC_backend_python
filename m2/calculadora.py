852810eb97d6df720004f12d502fc392b9910140
dannydays <danieldiasjanovicci@gmail.com>
2025-04-07 10:23:03 -0300

    correcoes

--- m2/calculadora.py
+++ m2/calculadora.py
@@ -6,23 +6,10 @@ def obter_numero(mensagem):
         except ValueError:
             print(f"Erro: {ValueError}")
 
-def soma(x, y):
-    return x+y
-
-def subtracao(x, y):
-    return x-y
-
-def multiplicacao(x, y):
-    return x*y
-
-def divisao(x, y):
-    return x/y
-
 def menu_operacoes():
     print("\nEscolha uma operação: ")
     operacoes = ["Soma", "Subtração", "Multiplicação", "Divisão"]
-    for i, operacao in enumerate(operacoes, 1):
-        print(f"{i} - {operacao}")
+    [print(f'{n} - {_}') for n,_ in enumerate(operacoes, start=1)]
     escolha = input("Escolha uma opção: ")
     return escolha
 
@@ -31,21 +18,21 @@ def main():
         num1 = obter_numero("Insira o primeiro número: ")
         num2 = obter_numero("Insira o segundo número: ")
         escolha = menu_operacoes()
-
+        
         if escolha == '1':
-            operacao = soma
+            operacao = lambda a,b:a+b
             operacao_nome = "Soma"
         elif escolha == '2':
-            operacao = subtracao
+            operacao = lambda a,b:a-b
             operacao_nome = "Subtração"
         elif escolha == '3':
-            operacao = multiplicacao
+            operacao = lambda a,b:a*b
             operacao_nome = "Multiplicação"
         elif escolha == '4':
             while num2 == 0:
                 print("Divisor não pode ser zero: ")
                 num2 = obter_numero("Por favor, insira outro número para o divisor: ")
-            operacao = divisao
+            operacao = lambda a,b:a/b
             operacao_nome = "Divisão"
         else:
             print("Opção inválida!")
@@ -56,11 +43,19 @@ def main():
             print(ZeroDivisionError)
         else:
             print(f"O resultado da {operacao_nome} é: {resultado}")
-        
-        continuar = input("Deseja realizar outra operação? (S/N): ").strip().upper()
-        if continuar != 'S':
-            print("Encerrando...")
-            break
 
+        while True:
+            continuar = input("Deseja realizar outra operação? (S/N): ").strip().upper()
+            if continuar == 'S':
+                break
+            if continuar == 'N':
+                print("Encerrando...")
+                quit()
+            else:
+                print("Opção inválida!")
+                continue
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nEncerrando...")
